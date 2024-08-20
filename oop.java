8667c3d2e1f1bff80d20713172b80f8b5d9cd0d8
muhammadtalal8 <muhammadtalalshakeel2009@gmail.com>
2024-08-20 16:42:08 +0500

    first commit

---
+++ oop.java
@@ -0,0 +1,17 @@
+class Pen {
+    String color;
+    String type;
+    public void wirte(){
+        System.out.println("writing something");
+    }
+}
+public class OOPS {
+    public static void main(String args[]){
+        Pen pen1 = new Pen();
+        pen1.color = "blue";
+        pen1.type = "gel";
+
+        pen1.write();
+    }
+}
+
